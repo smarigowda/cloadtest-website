a01222d17fe0427a3db60a0a976f2c7905e3765d
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 19:16:06 +0100

    adding scss support

--- src/components/Header/Component.tsx
+++ src/components/Header/Component.tsx
@@ -1,13 +1,11 @@
-import styles from './header.module.css';
+import styles from './header.module.scss';
 const Header = function() {
   return (
     <header className={styles.header}>
-      <div className="header-top">
-      
-      
-        <div className="contact-phone">
+      <div className={styles.headerTop}>
+        <div className={styles.contactPhone}>
           <i className="fas fa-phone"></i>
-          07920830031
+          <div>07920830031</div>
           </div>
         <div className="email">Send EMail</div>
       </div>
