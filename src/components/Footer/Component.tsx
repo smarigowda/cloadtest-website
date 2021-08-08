06aeb54afb26957ccf562593bd68fde19eb4e5fd
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 17:13:30 +0100

    big and bold...header

---
+++ src/components/Footer/Component.tsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <p>
+      Copyright &copy; 2021 CloadTest UK Limited. All rights reserved
+    </p>
+  )
+}
+
+export { Footer }
--- src/components/Header/Component.tsx
+++ src/components/Header/Component.tsx
@@ -2,7 +2,7 @@ import './header.css';
 const Header = function() {
   return (
     <header>
-      <h1>Header Goes Here...</h1>
+      <h1>Cload Test UK Limited</h1>
     </header>
   )
 }
