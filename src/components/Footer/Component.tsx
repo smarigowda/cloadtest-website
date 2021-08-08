bdc47498b2cb2ca0b112c25b8f77aac498f71c3b
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 17:56:08 +0100

    socials added

--- src/components/Footer/Component.tsx
+++ src/components/Footer/Component.tsx
@@ -2,9 +2,18 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <p>
-      Copyright &copy; 2021 CloadTest UK Limited. All rights reserved
-    </p>
+    <footer>
+      <p> Copyright &copy; 2021 CloadTest UK Limited</p>
+      <p> Apollo House Hallam Way, Whitehills Business Park, Blackpool, England, FY4 5FS </p>
+      <div className="socials">
+        <a href="/">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="/">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        </div>
+    </footer>
   )
 }
 
