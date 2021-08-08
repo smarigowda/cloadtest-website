1119cc53b3727235242d51cb9b651096bfd132a2
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 19:59:16 +0100

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css';
-import { Header } from 'components/Header';
-import { Footer } from 'components/Footer';
+import "./App.css";
+import { Header } from "components/Header";
+import { Footer } from "components/Footer";
+import { Hero } from "components/Hero";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Hero />
       <Footer />
     </div>
   );
