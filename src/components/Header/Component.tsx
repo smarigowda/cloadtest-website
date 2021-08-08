f2aeed708a08fb24c99e397ff4eec9b779396e65
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 18:18:13 +0100

    added css module support

--- src/components/Header/Component.tsx
+++ src/components/Header/Component.tsx
@@ -1,7 +1,16 @@
-import './header.css';
+import styles from './header.module.css';
 const Header = function() {
   return (
-    <header>
+    <header className={styles.header}>
+      <div className="header-top">
+      
+      
+        <div className="contact-phone">
+          <i className="fas fa-phone"></i>
+          07920830031
+          </div>
+        <div className="email">Send EMail</div>
+      </div>
       <h1>Cload Test UK Limited</h1>
     </header>
   )
