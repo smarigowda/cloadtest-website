324c42447577f548fcf220850fa674b42c354b12
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 19:25:58 +0100

    footer style converted to scss

--- src/components/Footer/Component.tsx
+++ src/components/Footer/Component.tsx
@@ -1,11 +1,11 @@
-import './footer.css';
+import styles from './footer.module.scss';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p> Copyright &copy; 2021 CloadTest UK Limited</p>
       <p> Apollo House Hallam Way, Whitehills Business Park, Blackpool, England, FY4 5FS </p>
-      <div className="socials">
+      <div className={styles.socials}>
         <a href="/">
           <i className="fab fa-facebook-f"></i>
         </a>
