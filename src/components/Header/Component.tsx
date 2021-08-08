919623c1ad0b9ad819755060ee18095865a3202a
Santosh Marigowda <santosharakere@gmail.com>
2021-08-08 20:17:21 +0100

    company name, no space

--- src/components/Header/Component.tsx
+++ src/components/Header/Component.tsx
@@ -1,17 +1,17 @@
-import styles from './header.module.scss';
-const Header = function() {
+import styles from "./header.module.scss";
+const Header = function () {
   return (
     <header className={styles.header}>
       <div className={styles.headerTop}>
         <div className={styles.contactPhone}>
           <i className="fas fa-phone"></i>
           <div>07920830031</div>
-          </div>
+        </div>
         <div className="email">Send EMail</div>
       </div>
-      <h1>Cload Test UK Limited</h1>
+      <h1>CloadTest UK Limited</h1>
     </header>
-  )
-}
+  );
+};
 
-export { Header }
+export { Header };
